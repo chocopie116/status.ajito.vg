1f3dda24db11f9688407c82607a759386e42f85c
chocopie116 <chocopie116@gmail.com>
2016-11-23 23:30:00 +0900

    ok

---
+++ server/index.js
@@ -0,0 +1,17 @@
+var twitter = require('twitter');
+
+var client = new twitter({
+    consumer_key        : process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret     : process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key    : process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret : process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+exports.handler = function(event, context) {
+    //いつも同じ文字列だとtwitterに投稿できないため
+    var random = Math.random().toString(36).slice(-8);
+    client.post('statuses/update', {status: random },  function(error, tweet, response) {
+        if(error) throw error;
+        console.log('done');
+    });
+}
