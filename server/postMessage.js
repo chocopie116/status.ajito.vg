e598c6d130fb73f748db2cfd6fce426c03c4dbd7
chocopie116 <chocopie116@gmail.com>
2016-11-28 00:21:18 +0900

    ok

--- server/postMessage.js
+++ server/postMessage.js
@@ -1,10 +1,8 @@
 var message = require('./message');
 
 exports.handler = function(event, context, callback) {
-    message.storeMessage(event.color, event.message)
+    message.storeMessage(event.color, event.message, event.user)
         .then(function(data) {
             callback(null, {reuslt: 'ok'});
-        }).catch(function(err) {
-            callback(null, {result: 'ng'});
         });
 }
