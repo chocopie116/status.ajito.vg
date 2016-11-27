132de92046648245a04f2258093aded7aede1b33
chocopie116 <chocopie116@gmail.com>
2016-11-27 23:48:41 +0900

    とりあえずできた

--- server/postMessage.js
+++ server/postMessage.js
@@ -1,9 +1,10 @@
 var message = require('./message');
 
 exports.handler = function(event, context, callback) {
-    var color = 'yellow';
-    var text = '今いる?';
-    message.storeMessage(color, text).then(function(data) {
-        callback(null, data);
-    });
+    message.storeMessage(event.color, event.message)
+        .then(function(data) {
+            callback(null, {reuslt: 'ok'});
+        }).catch(function(err) {
+            callback(null, {result: 'ng'});
+        });
 }
