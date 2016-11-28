91de7c431c29de8ac20a776bd913872a20558a4f
chocopie116 <chocopie116@gmail.com>
2016-11-28 23:54:48 +0900

    削除

--- server/postSlack.js
+++ server/postSlack.js
@@ -4,9 +4,7 @@ exports.handler = function(event, context, callback) {
     //slackのコマンドの引数にokが入っている場合は-1以外になる
     //var containsOkCommand = event.text.indexOf('ok') != -1;
     //var color = (containsOkCommand) ? 'green' : 'red';
-
-    message.storeMessage('green')
-        .then(function(data) {
-            callback(null, {reuslt: 'ok'});
+    message.storeMessage('green').then(function() {
+            callback(null, null);
         });
 }
