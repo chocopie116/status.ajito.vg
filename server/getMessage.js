62356a1a9d137f75105370b7af31f2cc4d2434ed
chocopie116 <chocopie116@gmail.com>
2016-12-11 23:52:50 +0900

    deleted unused server code

--- server/getMessage.js
+++ server/getMessage.js
@@ -1,21 +1,6 @@
-//var twitter = require('twitter');
 var message = require('./lib/message');
 
-//var client = new twitter({
-//    consumer_key        : process.env.TWITTER_CONSUMER_KEY,
-//    consumer_secret     : process.env.TWITTER_CONSUMER_SECRET,
-//    access_token_key    : process.env.TWITTER_ACCESS_TOKEN_KEY,
-//    access_token_secret : process.env.TWITTER_ACCESS_TOKEN_SECRET
-//});
-
 exports.handler = function(event, context, callback) {
-    //いつも同じ文字列だとtwitterに投稿できないため
-    //var random = Math.random().toString(36).slice(-8);
-    //client.post('statuses/update', {status: random },  function(error, tweet, response) {
-    //    if(error) throw error;
-    //    console.log('done');
-    //});
-
     message.readMessages().then(function(data) {
         callback(null, data);
     });
