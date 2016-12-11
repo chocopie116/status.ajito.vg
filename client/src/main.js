fe30bb20bab0ef30d0ded932a1a9a27df8ac6c84
chocopie116 <chocopie116@gmail.com>
2016-12-11 21:27:44 +0900

    enable react

--- client/src/main.js
+++ client/src/main.js
@@ -1,3 +1,18 @@
-var react = require('react');
+var React = require('react');
+var ReactDom = require('react-dom');
 
 
+var MessageBox = React.createClass({
+    render: function() {
+        return (
+            <div>
+                <h1>test</h1>
+            </div>
+        )
+    }
+});
+
+ReactDom.render(
+    <MessageBox />,
+    document.querySelector('#js-content')
+);
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,18 @@ module.exports = {
     output: {
         filename: 'public/dist/bundle.js'
     },
+    module: {
+        loaders: [
+            {
+                test: /.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
     resolve: {
         extensions: ['', '.js']
     }
